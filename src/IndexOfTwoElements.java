962bb6b55f5f1d1211b8eb284f77ac6207bc728c
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-01 22:58:50 -0700

    added comments for better understandings.

--- src/IndexOfTwoElements.java
+++ src/IndexOfTwoElements.java
@@ -1,15 +1,23 @@
 public class IndexOfTwoElements {
 
+    //a method which returns the indexes of two numbers whose sum is equal to the target,
+    //given array is sorted and stored only one solution
     public static int [] indexOfTwoElements(int [] arr, int target){
+        //get an array to be returned
         int resultant[] = new int[2];
+        //two pointers
         int i = 0;
         int j= arr.length -1;
+
+        //until i meets j
         while (i<j){
+            //if their sum is equals to target then add the indexes in resultant
             if(arr[i] + arr[j] == target){
                 resultant[0] = i+1;
                 resultant[1] = j+1;
                 break;
             }
+            // if not, then check if ith is greater or jth, then move on.
             if(arr[i]<arr[j]){
                 i++;
             }else{
