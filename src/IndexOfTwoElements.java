55a1a75c1ecb2179934b8dfea38587c078844073
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-01 22:55:56 -0700

    created a method which returns the indexes of two numbers whose sum is equals to the target. Also the given array is sorted and there will only be one solution stored in array.

--- src/IndexOfTwoElements.java
+++ src/IndexOfTwoElements.java
@@ -1,2 +1,25 @@
 public class IndexOfTwoElements {
+
+    public static int [] indexOfTwoElements(int [] arr, int target){
+        int resultant[] = new int[2];
+        int i = 0;
+        int j= arr.length -1;
+        while (i<j){
+            if(arr[i] + arr[j] == target){
+                resultant[0] = i+1;
+                resultant[1] = j+1;
+                break;
+            }
+            if(arr[i]<arr[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }return resultant;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
